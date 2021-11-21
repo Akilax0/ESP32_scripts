b4c564b5d791894e8ab3f459399d65bb29b247c4
Akilax0 <killerilaksha@gmail.com>
2021-11-21 09:16:39 +0530

    Queue ino

--- FreeRTOS/queue/queue.ino
+++ FreeRTOS/queue/queue.ino
@@ -30,6 +30,8 @@ void printMessages(void *parameters){
   int item;
 
   while(1){
+    //checks if msg in queue
+    //args(queue handler, address of local variable queue item gets copied to, time parameter(no of ticks))
     if(xQueueReceive(msg_queue, (void *)&item, 0)==pdTRUE){
       Serial.println(item);
       }
@@ -48,31 +50,35 @@ void setup() {
   // Wait a moment to start (so we don't miss Serial output)
   vTaskDelay(1000 / portTICK_PERIOD_MS);
   Serial.println();
-  Serial.println("---FreeRTOS Heap Demo---");
-  Serial.println("Enter a string");
+  Serial.println("---FreeRTOS Queue Demo---");
+
+  //Create Queue
+  //args(no of items, size of each)
+  msg_queue = xQueueCreate(msg_queue_len, sizeof(int));
 
   // Start Serial receive task
-  xTaskCreatePinnedToCore(readSerial,
-                          "Read Serial",
+  xTaskCreatePinnedToCore(printMessages,
+                          "print messages",
                           1024,
                           NULL,
                           1,
                           NULL,
                           app_cpu);
 
-  // Start Serial print task
-  xTaskCreatePinnedToCore(printMessage,
-                          "Print Message",
-                          1024,
-                          NULL,
-                          1,
-                          NULL,
-                          app_cpu);
-  
-  // Delete "setup and loop" task
-  vTaskDelete(NULL);
+
 }
 
 void loop() {
   // Execution should never get here
+  static int num =0;
+
+  //Try to add item for 10 tick else fail
+  if(xQueueSend(msg_queue, (void*)&num, 10)!=pdTRUE){
+    Serial.println("Queue Full");
+    }
+
+    //Generally seraial input output handled by one task
+    num ++;
+
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
 }
